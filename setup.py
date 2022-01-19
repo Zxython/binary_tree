34522d6114a4e29fd48223e6f6f5cee55e1a125b
Zxython <98050815+Zxython@users.noreply.github.com>
2022-01-19 12:45:06 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='binary_tree',
+      version='1.0',
+      description='Adds a binary tree datatype to python',
+      url='https://github.com/Zxython/binary_tree_v1',
+      author='Jerry Gillian',
+      author_email='jerrygillian822@gmail.com',
+      license='Free to ise',
+      packages=['binary_tree'],
+      zip_safe=False)
